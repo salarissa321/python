2220e1e2fcfaefcf9df2ce486ab74cb878e04ec9
salarissa321 <salarissa321@gmail.com>
2023-02-19 14:44:50 +0100

    add 1 file

---
+++ python/de.py
@@ -0,0 +1,13 @@
+x= 5
+y = 10
+if x== 5 and y==10:
+    print ('true')
+
+if x==5 or y==12 :
+    print ('true')
+
+if not (x==6 or y==10):
+    print ('true')
+if x !=4 and y!= 6:
+    print ('true')
+
---
+++ python/pa.py
@@ -0,0 +1,253 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x = 5
+x2=10
+2x=3
+SyntaxError: invalid decimal literal
+
+f="mystro"
+h='mystro'
+f
+'mystro'
+h
+'mystro'
+
+2/2
+1.0
+2**5
+32
+
+x= 5
+X=10
+x
+5
+X
+10
+
+if return break yield
+SyntaxError: invalid syntax
+
+x=5
+s='welcome'
+x
+5
+s
+'welcome'
+x , y = 5, 3
+a = b = c =1
+x
+5
+y
+3
+a
+1
+b
+1
+c
+1
+
+x=5 ; y=10
+x
+5
+y
+10
+
+k='5'
+k
+'5'
+"5" * "2"
+Traceback (most recent call last):
+  File "<pyshell#37>", line 1, in <module>
+    "5" * "2"
+TypeError: can't multiply sequence by non-int of type 'str'
+
+True False
+SyntaxError: incomplete input
+
+
+f= (1,2,3,)
+f
+(1, 2, 3)
+g = {1:"ahmad" , 2:"ali"}
+g
+{1: 'ahmad', 2: 'ali'}
+
+type(f)
+<class 'tuple'>
+type(g)
+<class 'dict'>
+
+g= "5"
+type(g)
+<class 'str'>
+
+f= ("5")
+type(f)
+<class 'str'>
+
+f=("5",)
+type(5)
+<class 'int'>
+
+f= ('5' ,)
+type(f)
+<class 'tuple'>
+
+y = ('tr' , 1)
+type (y)
+<class 'tuple'>
+
+
+u= (True)
+type (u)
+<class 'bool'>
+<class 'bool'>
+SyntaxError: invalid syntax
+u= (True,)
+type(u)
+<class 'tuple'>
+
+
+4+5
+9
+4/2
+2.0
+2**4
+16
+
+9/3
+3.0
+9/4
+2.25
+
+
+9//4
+2
+
+
+x=2.0
+type(x)
+<class 'float'>
+
+9%4
+1
+11%3
+2
+
+5>6
+False
+4>2
+True
+
+8>=0
+True
+x
+2.0
+
+print (x)
+2.0
+
+x=5
+x==6
+False
+
+5!= 7
+True
+i++
+SyntaxError: incomplete input
+
+i+=1
+Traceback (most recent call last):
+  File "<pyshell#105>", line 1, in <module>
+    i+=1
+NameError: name 'i' is not defined. Did you mean: 'id'?
+
+5=1
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+x=5
+x +=1
+x
+6
+
+x*=5
+x
+30
+
+x /=6
+x
+5.0
+
+x**=3
+x
+125.0
+
+x**2
+15625.0
+
+x**3
+1953125.0
+x=2
+x**2
+4
+x**3
+8
+
+================ RESTART: C:/Users/faten2/OneDrive/Desktop/de.py ===============
+done
+
+================ RESTART: C:/Users/faten2/OneDrive/Desktop/de.py ===============
+default
+
+================ RESTART: C:/Users/faten2/OneDrive/Desktop/de.py ===============
+welcome
+
+================ RESTART: C:/Users/faten2/OneDrive/Desktop/de.py ===============
+
+================ RESTART: C:/Users/faten2/OneDrive/Desktop/de.py ===============
+
+================ RESTART: C:/Users/faten2/OneDrive/Desktop/de.py ===============
+welcome
+
+================ RESTART: C:/Users/faten2/OneDrive/Desktop/de.py ===============
+
+================ RESTART: C:/Users/faten2/OneDrive/Desktop/de.py ===============
+welcome
+>>> 
+================ RESTART: C:/Users/faten2/OneDrive/Desktop/de.py ===============
+welcome
+>>> 
+================ RESTART: C:/Users/faten2/OneDrive/Desktop/de.py ===============
+welcome
+welcome
+>>> 
+================ RESTART: C:/Users/faten2/OneDrive/Desktop/de.py ===============
+welcome ahmad
+>>> 
+================ RESTART: C:/Users/faten2/OneDrive/Desktop/de.py ===============
+welcome salar
+>>> 
+================ RESTART: C:/Users/faten2/OneDrive/Desktop/de.py ===============
+......
+>>> 
+================ RESTART: C:/Users/faten2/OneDrive/Desktop/de.py ===============
+welcome
+>>> 
+================ RESTART: C:/Users/faten2/OneDrive/Desktop/de.py ===============
+welcome
+welcome
+>>> 
+================ RESTART: C:/Users/faten2/OneDrive/Desktop/de.py ===============
+true
+>>> 
+================ RESTART: C:/Users/faten2/OneDrive/Desktop/de.py ===============
+true
+true
+>>> 
+================ RESTART: C:/Users/faten2/OneDrive/Desktop/de.py ===============
+true
+true
+>>> 
+================ RESTART: C:/Users/faten2/OneDrive/Desktop/de.py ===============
+true
+true
+true
