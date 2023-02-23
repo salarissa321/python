461307b5dac7d8c2134c68b618fc51b5cfa67e19
salarissa321 <salarissa321@gmail.com>
2023-02-23 13:52:45 +0100

    add python Basics

---
+++ python-Basics/python Basics.py
@@ -0,0 +1,31 @@
+
+
+# muoduler
+# arguments , parameters
+# requierd
+# keyword : positional
+# default
+
+def mysum(y,x=0):
+    
+    return x+y
+
+
+l = mysum(3)
+print (l)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
